e5564124f52e8ea04a0c79f4d960c3eb73634c4d
Firoozeh Saiepour <fsaiepour@gmail.com>
2021-01-02 18:16:23 +0000

    Remove extra files

--- build/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sayHello = void 0;
-var myName = process.env.NAME || 'UNKNOWN';
-exports.sayHello = function (name) {
-    return "Hello " + name + "!";
-};
-console.log(exports.sayHello(myName));
